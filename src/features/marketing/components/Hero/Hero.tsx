b1f0953c500dec99d8c9e43115b4f6458b21e994
Gigalord <dev.elbehery@gmail.com>
2024-12-06 23:29:54 +0200

    remove errors and style the hero section

--- src/features/marketing/components/Hero/Hero.tsx
+++ src/features/marketing/components/Hero/Hero.tsx
@@ -2,35 +2,25 @@
 
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import { motion } from "framer-motion";
+import "./hero.css";
 
 export default function Hero() {
   return (
     <section>
-      <Test />
+      <AuroraBackground className="hero text-white">
+        <motion.div
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+        >
+          <p className="hero-head">Background lights are cool you know.</p>
+          <p className="hero-paragraph">And this, is chemical burn.</p>
+        </motion.div>
+      </AuroraBackground>{" "}
     </section>
   );
 }
-
-const Test = () => {
-  return (
-    <AuroraBackground className="text-white">
-      <motion.div
-        className="relative flex flex-col gap-4 items-center justify-center px-4"
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-      >
-        <p className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-          Background lights are cool you know.
-        </p>
-        <p className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-          And this, is chemical burn.
-        </p>
-      </motion.div>
-    </AuroraBackground>
-  );
-};
