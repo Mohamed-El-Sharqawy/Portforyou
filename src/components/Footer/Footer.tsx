2356c6f9e9b6a305200a5c0cfc267a071a0b7e2a
Gigalord <dev.elbehery@gmail.com>
2024-12-07 21:27:00 +0200

    delete logo from footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,10 @@
 import Link from "next/link";
-import Logo from "../Logo";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className="border-t py-12 font-sans mx-auto px-4 space-y-4 text-center">
-      <div className="mb-12 w-fit mx-auto font-serif">
-        <Logo scrollToTop />
-      </div>
-
       <p>
         Made with ❤️ and dedication by{" "}
         <Link
