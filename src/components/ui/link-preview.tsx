d2d24291ace80f4e8c6cf3a994483bb543a3082d
Gigalord <dev.elbehery@gmail.com>
2024-12-08 22:20:03 +0200

    fix ts issue in link preview

--- src/components/ui/link-preview.tsx
+++ src/components/ui/link-preview.tsx
@@ -67,7 +67,6 @@ export const LinkPreview = ({
 
   const translateX = useSpring(x, springConfig);
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleMouseMove = (event: any) => {
     const targetRect = event.target.getBoundingClientRect();
     const eventOffsetX = event.clientX - targetRect.left;
