2d690591409303b25c454670dc1f48ee84646bba
Gigalord <dev.elbehery@gmail.com>
2024-12-08 22:34:46 +0200

    add id to pricings section

--- src/features/marketing/components/Pricings/Pricings.tsx
+++ src/features/marketing/components/Pricings/Pricings.tsx
@@ -3,7 +3,7 @@ import PricingsGrid from "./PricingsGrid";
 
 export default function Pricing() {
   return (
-    <section className="marketing-section space-y-20">
+    <section id="pricings" className="marketing-section space-y-20">
       <SectionsHeading text="Pricings" />
 
       <PricingsGrid />
