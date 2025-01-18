738b2ae81821d038ef024546c8ed8ba4c7fa86a7
Gigalord <dev.elbehery@gmail.com>
2025-01-18 02:21:54 +0200

    in-case sensitive bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import { ClerkLoaded, ClerkLoading, ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import type { Metadata } from "next";
 
-import Header from "@/components/header/header";
-import Footer from "@/components/footer/footer";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 import "@/styles/globals.css";
 
